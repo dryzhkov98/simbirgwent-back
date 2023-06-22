ceec4e9a2410d3c678ec11cc3f60b9a9173d2469
Lev <nefedov.lewa@gmail.com>
2023-06-22 19:52:03 +0400

    fix: remove test module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config/app-config.module';
 import { AppConfig } from './config/app.config';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AppConfigModule.forRoot(AppConfig), UsersModule],
