0004698059d4da83bf3795e1e45e21d26d94bf8e
d.ryzhkov <d.ryzhkov@atomyze.ru>
2023-07-02 14:23:15 +0300

    feat: fix logger config, fix prisma credentinals

--- src/config/logger/logger.config.ts
+++ src/config/logger/logger.config.ts
@@ -1,7 +1,10 @@
 import { format, transports } from 'winston';
-import { LoggerModule } from './logger.module';
 import { WinstonModuleOptions } from 'nest-winston';
 import 'winston-daily-rotate-file';
+import { AppConfigService } from '../app-config/app-config.service';
+import { AppConfig } from '../app.config';
+
+const config = new AppConfigService(AppConfig);
 
 export function getConfigLogger(): WinstonModuleOptions {
   return {
@@ -25,10 +28,10 @@ export function getConfigLogger(): WinstonModuleOptions {
         format: format.combine(
           format.cli({ all: true, colors: { info: 'green', error: 'red' } }),
           format.splat(),
-          format.timestamp({ format: new Date().toLocaleString() }),
+          format.timestamp({ format: new Date().toLocaleString('ru-Ru') }),
           format.printf((info) => {
-            const appName = 'SimbirGwent';
-            return `[${appName}] ${info.level} ${info.timestamp}  [${LoggerModule.name}] : ${info.message}`;
+            const appName = config.get<string>('NAME');
+            return `[${appName}] ${info.level} ${info.timestamp}${info.message}`;
           }),
         ),
       }),
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [DatabaseService],
+  providers: [DatabaseService],
   exports: [DatabaseService],
 })
 export class DatabaseModule {}
