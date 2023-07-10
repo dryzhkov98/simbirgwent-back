847f1011bcab60b11733fdf88233233ee4a6d89e
Lev <nefedov.lewa@gmail.com>
2023-07-10 13:23:45 +0400

    fix: add metadata for swagger

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,23 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AuthService } from './auth.service';
-import { IResponse } from './interfaces/response.interface';
+import { AuthResponseDto } from './dto/auth-response.dto';
 import { Response } from 'express';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IResponse } from './interfaces/response.interface';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-up')
+  @ApiOperation({ summary: "User's registration" })
+  @ApiResponse({
+    status: 201,
+    type: AuthResponseDto,
+    description: 'User has been successfully registered',
+  })
   async signUp(
     @Body() createUserDto: CreateUserDto,
     @Res({ passthrough: true }) response: Response,
